031ac0ada26bbed11c6473de0e57a5ddd142972d
Rooobins <kai_cuckoo@foxmail.com>
2019-01-18 23:14:27 +0800

    示例代码

--- Test/test.py
+++ Test/test.py
@@ -872,4 +872,35 @@
 # print(text)
 
 #***********************************************************************************************************************
-print("hello world")
+# print("hello world")
+
+#消费者系统
+def consumer():
+    r=''
+    while True:
+        n=yield r
+        if not n:
+            return
+        print(n)
+        print('[CONSUMER] Consuming %s...'%(n))
+        r='200 OK'
+
+#生产者系统
+def produce(c):
+    c.send(None)
+    n=0
+    while n<5:
+        n=n+1
+        print('[PRODUCER] Producing %s...'%(n))
+        r=c.send(n)
+        print('[PRODUCER] Consumer return: %s'%(r))
+    c.close()
+
+
+
+
+
+
+if __name__=="__main__":
+    c=consumer()
+    produce(c)
